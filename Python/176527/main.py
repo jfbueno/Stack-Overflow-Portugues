37f5635da8fd94496911a3407135089ef347da25
Jéferson <buenojeferson@outlook.com>
2018-01-19 13:27:31 -0200

    https://pt.stackoverflow.com/q/176527

---
+++ Python/176527/main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from module import *
+
+print foo()
+print bar
+
+# A linha abaixo causará uma exception, porque o membro não foi exportado
+print baz
---
+++ Python/176527/module.py
@@ -0,0 +1,6 @@
+__all__ = ['foo', 'bar']
+
+baz = 5
+bar = 10
+def foo(): 
+    return 'qualquer coisa'
